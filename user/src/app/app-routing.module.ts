be40d92a5371dfb5284e2fd1b7470132a107fe40
Shrutigupta03 <shrutigupta70240@gmail.com>
2023-09-12 21:45:12 +0530

    updated

--- user/src/app/app-routing.module.ts
+++ user/src/app/app-routing.module.ts
@@ -1,10 +1,11 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CrouselComponent } from './crousel/crousel.component';
+import {HomeComponent} from './home/home.component';
 
 const routes: Routes = [
-  // { path:'', component:HomeComponent},
-  { path:'crousel', component:CrouselComponent}
+  { path:'home', component: HomeComponent},
+  { path:'crousel', component:CrouselComponent }
     
 ];
 
