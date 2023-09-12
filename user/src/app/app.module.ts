981f3d1dec2799953b1f39c7791fbfca01339d75
Shrutigupta03 <shrutigupta70240@gmail.com>
2023-09-12 11:40:14 +0530

    components added

--- user/src/app/app.module.ts
+++ user/src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HomeComponent } from './home/home.component';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { FooterComponent } from './footer/footer.component';
+import { ImageComponent } from './image/image.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    AboutUsComponent,
+    FooterComponent,
+    ImageComponent
   ],
   imports: [
     BrowserModule,
