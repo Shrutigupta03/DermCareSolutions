b99498bbc1c9416c612883288ebb48fe025fd1bb
Prince Sahu <princesahu169@gmail.com>
2023-09-20 12:20:24 +0530

    Revert "Merge branch 'main' of https://github.com/Shrutigupta03/DermCareSolutions"
    This reverts commit fe8f8b1fa501740efae1705544426b86f9c92099, reversing
    changes made to 28bfb5c6f09fdef1f53de248d091b2b39c8be41b.

--- app.py
+++ app.py
@@ -3,41 +3,27 @@ from flask import Flask, request, render_template
 import tensorflow as tf
 import numpy as np
 import cv2
+app = Flask(__name__, template_folder="templates")
+# UPLOAD_FOLDER = "/config/workspace/images" 
+UPLOAD_FOLDER = "images" 
+model = tf.keras.models.load_model("model/dermo_8th.h5")
 
-app = Flask(__name__)
-UPLOAD_FOLDER = "/config/workspace/images"
-model = tf.keras.models.load_model("/config/workspace/model/SkinCancer.h5")
-class_names = [
-    "Actinic keratoses and intraepithelial carcinomae",
-    "basal cell carcinoma",
-    "benign keratosis-like lesions",
-    "dermatofibroma",
-    "melanocytic nevi",
-    "pyogenic granulomas and hemorrhage",
-    "melanoma"
-]
+class_names = ["Acne", "Eczema", "Atopic", "Psoriasis", "Tinea", "Vitiligo"]
 
-def predict_skin_disease(image_path, model):
-    # Load and preprocess image
-    original_image = cv2.imread(image_path)
+def predict_skin_disease( image_path, model):
+    try:
+        
+        img = cv2.imread(image_path)
+        img = cv2.resize(img, (128, 128))
 
-    if original_image is None:
-        return "Invalid Image"
+        pred = model.predict(img)
+        predicted_class_index = np.argmax(pred)
+        predicted_class_name = class_names[predicted_class_index]
 
-    resized_image = cv2.resize(original_image, (28, 28))
-    batch_size = 4
-    batch_of_images = np.array([resized_image] * batch_size)
-
-    # Make prediction on preprocessed image
-    pred = model.predict(batch_of_images)
-    predicted_class = np.argmax(pred, axis=1)
-    predicted_class_number = predicted_class[0]
-
-    if predicted_class_number < len(class_names):
-        predicted_class_name = class_names[predicted_class_number]
         return predicted_class_name
 
-    return "Unknown"
+    except Exception as e:
+        return "Error: " + str(e)
 
 @app.route("/", methods=["GET", "POST"])
 def upload_predict():
@@ -51,7 +37,7 @@ def upload_predict():
             image_file.save(image_location)
             pred = predict_skin_disease(image_location, model)
             return render_template('index.html', prediction=pred)
-    return render_template('index.html', prediction=None)
+    return render_template('index.html', prediction=0)
 
 if __name__ == "__main__":
     app.run(port=5000, host="0.0.0.0", debug=True)
